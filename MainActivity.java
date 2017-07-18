863459630221f8ac056183e5e84f4b2ced8af285
orklar <okadaifciler15@ku.edu.tr>
2017-07-18 12:25:06 +0300

    version 2

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package oguzhankada.chatapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //firebase initialization
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+}
---
+++ register.java
@@ -0,0 +1,20 @@
+package oguzhankada.chatapp2;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+}
